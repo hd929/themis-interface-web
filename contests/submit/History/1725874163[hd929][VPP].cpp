3aff13c1d57640e0889b4273a235339235d38faf
Akatsuki <handuy099@gmail.com>
2024-09-15 12:01:34 +0700

    ignore: history and penalty

--- contests/submit/History/1725874163[hd929][VPP].cpp
+++
@@ -1,8 +0,0 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-int main() {
-  freopen("VPP.OUT", "w", stdout);
-  printf("52 81 12 2");
-  return 0;
-}
--- contests/submit/History/hd929/$History/20240909164230708_063861583310066[hd929][VPP].cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main() {
-  printf("52 81 12 2");
-  return 0;
-}
